d2d924091077b811c42a8449647037e1d0f7581f
taken <mail@taken.dev>
2025-10-18 03:32:57 +0200

    FIX: code was looking bad

--- win.cpp
+++ win.cpp
@@ -1,34 +1,24 @@
 #include <windows.h>
 
-const int HOTKEY_ID = 1;
-
-// WINDOW SIZE
-const int T_WIDTH = 1280;
-const int T_HEIGHT = 720;
-
 int main() {
-	if (RegisterHotKey(NULL, HOTKEY_ID, MOD_CONTROL | MOD_ALT, 0x4C) == 0) 
-	{
-        MessageBoxW(NULL, L"Failed to register hotkey.", L"Error", MB_ICONERROR);
+    constexpr int HOTKEY_ID = 1, WIDTH = 1280, HEIGHT = 720;
+    
+    if (!RegisterHotKey(NULL, HOTKEY_ID, MOD_CONTROL | MOD_ALT, 0x4C)) {
         return 1;
     }
-	
-	const int T_X = (GetSystemMetrics(SM_CXSCREEN) - T_WIDTH) / 2;
-	const int T_Y = (GetSystemMetrics(SM_CYSCREEN) - T_HEIGHT) / 2;
+    
+    const int x = (GetSystemMetrics(SM_CXSCREEN) - WIDTH) / 2;
+    const int y = (GetSystemMetrics(SM_CYSCREEN) - HEIGHT) / 2;
 
-	MSG msg;
-
-	while(GetMessage(&msg, NULL, 0, 0) > 0)
-	{
-		if (msg.message == WM_HOTKEY && msg.wParam == HOTKEY_ID)
-		{
-            SetWindowPos(GetForegroundWindow(), NULL, T_X, T_Y, T_WIDTH, T_HEIGHT, SWP_NOZORDER | SWP_NOACTIVATE);
-        }
-		
-		TranslateMessage(&msg);
-        DispatchMessage(&msg);
-	}
-	
-	UnregisterHotKey(NULL, HOTKEY_ID);
-	return 0;
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        if (msg.message == WM_HOTKEY && msg.wParam == HOTKEY_ID)
+            SetWindowPos(GetForegroundWindow(), NULL, x, y, WIDTH, HEIGHT, SWP_NOZORDER | SWP_NOACTIVATE);
+        
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+    
+    UnregisterHotKey(NULL, HOTKEY_ID);
+    return 0;
 }
